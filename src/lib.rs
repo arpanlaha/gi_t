10902f23523af9c7c6ab9c266cac4746e419e888
Arpan Laha <arpanlaha99@gmail.com>
2021-03-04 20:19:35 +0000

    split functionality into lib

---
+++ src/lib.rs
@@ -0,0 +1,107 @@
+#![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
+
+use std::{
+    fmt::{Display, Formatter, Result as FmtResult},
+    process::{Command, Stdio},
+};
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
+
+pub fn process_args(args: impl Iterator<Item = String>) -> Result<(), GiError> {
+    let mut args: Vec<String> = args.collect();
+
+    let mut stderr = StandardStream::stderr(ColorChoice::Always);
+
+    if stderr
+        .set_color(ColorSpec::new().set_fg(Some(Color::Red)))
+        .is_err()
+    {
+        return Err(GiError::StderrSet);
+    }
+
+    if args.is_empty() {
+        return Err(GiError::NoArgs);
+    }
+
+    if !args[0].starts_with('t') {
+        return Err(GiError::BadPrefix);
+    }
+
+    args[0].remove(0);
+
+    if args[0].is_empty() {
+        args.remove(0);
+    }
+
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
+
+    if stdout
+        .set_color(ColorSpec::new().set_fg(Some(Color::Green)))
+        .is_err()
+    {
+        return Err(GiError::StdoutSet);
+    }
+
+    println!(
+        "Running `git{}{}`...",
+        if args.is_empty() { "" } else { " " },
+        args.join(" ")
+    );
+
+    if stdout.reset().is_err() {
+        return Err(GiError::StdoutReset);
+    }
+
+    stdout.reset().expect("Unable to reset stdout color!");
+
+    println!();
+
+    if Command::new("git")
+        .args(&args)
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .output()
+        .is_err()
+    {
+        return Err(GiError::GitFail);
+    }
+
+    Ok(())
+}
+
+#[derive(Debug, Clone)]
+pub enum GiError {
+    BadPrefix,
+    GitFail,
+    NoArgs,
+    StderrSet,
+    StdoutReset,
+    StdoutSet,
+}
+
+impl GiError {
+    pub fn print(self) {
+        StandardStream::stderr(ColorChoice::Always)
+            .set_color(ColorSpec::new().set_fg(Some(Color::Red)))
+            .expect("Unable to set stderr color!");
+
+        println!("{}", self)
+    }
+}
+
+impl Display for GiError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(
+            f,
+            "{}",
+            match &self {
+                Self::BadPrefix => "Argument does not start with a `t`!",
+                Self::GitFail => "Failed to run git!",
+                Self::NoArgs => "No arguments provided!",
+                Self::StderrSet => "Unable to set stderr color!",
+                Self::StdoutReset => "Unable to reset stdout color!",
+                Self::StdoutSet => "Unable to set stdout color!,",
+            }
+        )
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,8 @@
 #![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
 
-use std::{
-    env,
-    process::{self, Command, Stdio},
-};
-use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
+use gi_t::GiError;
+use std::env;
 
 fn main() {
-    let mut args: Vec<String> = env::args().skip(1).collect();
-
-    let mut stderr = StandardStream::stderr(ColorChoice::Always);
-    stderr
-        .set_color(ColorSpec::new().set_fg(Some(Color::Red)))
-        .expect("Unable to set stderr color!");
-
-    if args.is_empty() {
-        eprintln!("No arguments provided!");
-        process::exit(1);
-    }
-
-    if !args[0].starts_with('t') {
-        eprintln!("Argument does not start with a `t`!");
-        process::exit(1);
-    }
-
-    args[0].remove(0);
-
-    if args[0].is_empty() {
-        args.remove(0);
-    }
-
-    let mut stdout = StandardStream::stdout(ColorChoice::Always);
-
-    stdout
-        .set_color(ColorSpec::new().set_fg(Some(Color::Green)))
-        .expect("Unable to set stdout color!");
-
-    println!(
-        "Running `git{}{}`...",
-        if args.is_empty() { "" } else { " " },
-        args.join(" ")
-    );
-
-    stdout.reset().expect("Unable to reset stderr color!");
-
-    println!();
-
-    Command::new("git")
-        .args(&args)
-        .stdin(Stdio::inherit())
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .output()
-        .expect("Failed to run git!");
+    gi_t::process_args(env::args().skip(1)).unwrap_or_else(GiError::print);
 }
