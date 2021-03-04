be0d205b91d07e5d44b06e9abfd4e42dae106342
Arpan Laha <arpanlaha99@gmail.com>
2021-03-04 17:54:46 +0000

    add termcolor support

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,15 @@ use std::{
     env,
     process::{self, Command, Stdio},
 };
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 fn main() {
     let mut args: Vec<String> = env::args().skip(1).collect();
+
+    let mut stderr = StandardStream::stderr(ColorChoice::Always);
+    stderr
+        .set_color(ColorSpec::new().set_fg(Some(Color::Red)))
+        .expect("Unable to set stderr color!");
 
     if args.is_empty() {
         eprintln!("No arguments provided!");
@@ -22,15 +28,26 @@ fn main() {
         args.remove(0);
     }
 
+    let mut stdout = StandardStream::stdout(ColorChoice::Always);
+
+    stdout
+        .set_color(ColorSpec::new().set_fg(Some(Color::Green)))
+        .expect("Unable to set stdout color!");
+
     println!(
         "Running `git{}{}`...",
         if !args.is_empty() { " " } else { "" },
         args.join(" ")
     );
 
+    stdout.reset().expect("Unable to reset stderr color!");
+
+    println!("");
+
     Command::new("git")
         .args(&args)
         .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
         .output()
         .expect("Failed to run git!");
 }
