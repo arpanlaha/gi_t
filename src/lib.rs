4db0133821fc891bbe23da387d949a74c3d52426
Arpan Laha <arpanlaha99@gmail.com>
2021-03-08 21:10:50 +0000

    use ? and split into separate functions

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use std::{
 };
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
-/// Corrects arguments passed to `gi_t` into the corresponding `git` command.
+/// Corrects arguments passed to `gi_t` into the corresponding `git` command for execution.
 ///
 /// # Arguments
 /// * `args` - the arguments to the program in [`Vec`] form.
@@ -21,6 +21,44 @@ pub fn process_args(mut args: Vec<String>) -> Result<(), GiError> {
     // save original arguments
     let original = args.join(" ");
 
+    transform_args(&mut args)?;
+
+    let mut stdout = StandardStream::stdout(ColorChoice::Auto);
+
+    stdout
+        .set_color(ColorSpec::new().set_fg(Some(Color::Green)))
+        .map_err(|_| GiError::StdoutSet)?;
+
+    println!(
+        "Correcting `gi {}` to `git{}{}`...",
+        original,
+        if args.is_empty() { "" } else { " " },
+        args.join(" ")
+    );
+
+    stdout.reset().map_err(|_| GiError::StdoutReset)?;
+
+    println!();
+
+    Command::new("git")
+        .args(&args)
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .output()
+        .map_err(|_| GiError::GitFail)?;
+
+    Ok(())
+}
+
+/// Transformed arguments passed to `gi_t` into the corresponding `git` arguments.
+///
+/// # Arguments
+/// * `args` - the arguments to the program in [`Vec`] form.
+///
+/// # Errors
+/// If the function receives malformed arguments, the corresponding [`GiError`] will be returned.
+pub fn transform_args(args: &mut Vec<String>) -> GiResult {
     // expect some arguments (not just `gi`)
     if args.is_empty() {
         return Err(GiError::NoArgs);
@@ -37,41 +75,10 @@ pub fn process_args(mut args: Vec<String>) -> Result<(), GiError> {
         args.remove(0);
     }
 
-    let mut stdout = StandardStream::stdout(ColorChoice::Auto);
-
-    if stdout
-        .set_color(ColorSpec::new().set_fg(Some(Color::Green)))
-        .is_err()
-    {
-        return Err(GiError::StdoutSet);
-    }
-
-    println!(
-        "Correcting `gi {}` to `git{}{}`...",
-        original,
-        if args.is_empty() { "" } else { " " },
-        args.join(" ")
-    );
-
-    if stdout.reset().is_err() {
-        return Err(GiError::StdoutReset);
-    }
-
-    println!();
-
-    if Command::new("git")
-        .args(&args)
-        .stdin(Stdio::inherit())
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .output()
-        .is_err()
-    {
-        return Err(GiError::GitFail);
-    }
-
     Ok(())
 }
+
+type GiResult = Result<(), GiError>;
 
 /// An enum to describe different errors that could occur while executing `gi_t`.
 #[derive(Debug, Clone)]
